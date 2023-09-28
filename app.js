143386848814fe3770ce9d6ffe19f949f820b56b
HafidAmbardi <109598700+HafidAmbardi@users.noreply.github.com>
2023-09-29 04:11:54 +0700

    added new files

---
+++ app.js
@@ -0,0 +1,55 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+const hiddenElements2 = document.querySelectorAll(".hidden2");
+const hiddenElements3 = document.querySelectorAll(".hidden3");
+const hiddenElements4 = document.querySelectorAll(".hidden4");
+hiddenElements.forEach((el) => observer.observe(el));
+hiddenElements2.forEach((el) => observer.observe(el));
+hiddenElements3.forEach((el) => observer.observe(el));
+hiddenElements4.forEach((el) => observer.observe(el));
+
+const swiper = new Swiper(".swiper", {
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+$(document).ready(function () {
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
